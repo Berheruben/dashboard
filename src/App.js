f3bfcc7b505fcf89a0aaa82b8f4622c7cc0f5455
Berheruben <rubenberhe56@yourdomain.com>
2022-10-13 12:22:48 +0200

    setup project creating Components and page folders

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css';
-
+import {navbar, Footer, Sidebar, Theme, Navbar} from './components'
+import {Ecommerce, Orders, Calendar, Employees, Stacked,Pyramid,Customers,ColorPicker,Kanban,Area,Bar,Pie,Financial,ColorMapping,Editor,Line} from './pages'
 function App() {
+
+  const activeMenu = true
   return (
-    <h1 className="underline text-3xl">
-     app
-    </h1>
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zIndex:'1000'}}>
+          <TooltipComponent content="settings" position="top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background:'blue', borderRadius:'50%'}}>
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar/>
+           </div> 
+        ): (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+             <Sidebar/> 
+            </div>
+        )}
+        <div className={
+          `dark:bg-main-bg bg-main-bg min-h-screen md:ml-72 w-full ${ activeMenu ? 'md:ml-72 ' : 'flex-2'}`
+
+         }>
+           <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+             <Navbar/>
+             </div>  
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<Ecommerce/>} /> 
+              <Route path="/ecommerce" element={<Ecommerce />} />
+              {/* Pages */}
+              <Route path="/employess" element={<Employees />} />
+              <Route path="/order" element={<Orders />} />
+              <Route path="/customers" element={<Customers />} />
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+              {/* Charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+             
+            </Routes>
+            </div>
+      </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChartsHeader = () => {
+  return (
+    <div>H</div>
+  )
+}
+
+export default ChartsHeader
