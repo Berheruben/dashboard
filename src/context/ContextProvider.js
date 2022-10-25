21f7353309a401724c732175ee249a14f5ae5f7d
Berheruben <rubenberhe56@yourdomain.com>
2022-10-25 18:12:52 +0200

    implements components/charts

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,27 +1,41 @@
-import React, {createContext, useContext,useState} from 'react'
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
 const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-    chat: false,
-    cart:false,
-    userProfile: false,
-    Notification:false
-}
+export const ContextProvider = ({ children }) => {
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [theme, setTheme] = useState(false);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [isShuted, setIsShuted] = useState(false);
 
-export const ContextProvider = ({children}) =>{
-const [activeMenu, setActiveMenu] = useState(true);
-const [isClicked, setIsClicked] = useState(initialState)
-const handleClick = (clicked)=>{
-    setIsClicked({...initialState,[clicked]:true})
-}
-    return (
-     <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick}}>
-         {children}
-     </StateContext.Provider>
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
+  };
 
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem('colorMode', color);
+  };
 
-)
-}
-export const useStateContext =  () => useContext(StateContext)
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
+  return (
+   
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, theme, setTheme,isShuted, setIsShuted }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
